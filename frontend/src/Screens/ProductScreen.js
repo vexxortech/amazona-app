e6d71e93a6823e89d3955aff14e209efa3513c3f
muhammadzubairqadir <muhammadzubairqadir161@gmail.com>
2022-03-15 17:29:13 +0500

    share on github

---
+++ frontend/src/Screens/ProductScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ProductScreen (props) {
+
+    return <div>ProductScreen</div>
+}
+
+export default ProductScreen;
---
+++ frontend/src/data.js
@@ -0,0 +1,115 @@
+const data = {
+    products : [
+    {
+        name: '1/24 Land Crouser',
+        category: "1/24 scale models",
+        image: "/images/d1.jpg",
+        price: 25,
+        brand:'XLG',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: '1/24 Lamborghini Countach',
+        category: "1/24 scale models",
+        image: "/images/d2.jpg",
+        price: 25,
+        brand:'Bburago',
+        rating: 5,
+        numReviews: 20
+    },
+    {
+        name: '1/27 Chevrolet Colorado ZR2',
+        category: "1/27 scale models",
+        image: "/images/d3.jpg",
+        price: 33,
+        brand:'Maisto',
+        rating: 4,
+        numReviews: 15
+    },
+    {
+        name: '1/32 Audi R8 (v8 engine) 2010 ',
+        category: "1/32 scale models",
+        image: "/images/d4.jpg",
+        price: 4,
+        brand:'kinsmart',
+        rating: 4.6,
+        numReviews: 9
+    },
+    {
+        name: '1/32 GTR R35  ',
+        category: "1/32 scale models",
+        image: "/images/d5.jpg",
+        price: 6,
+        brand:'chinese',
+        rating: 4,
+        numReviews: 5
+    },
+    {
+        name: '1/36 Mercedes-benz SLS(AMG)',
+        category: "1/36 scale models",
+        image: "/images/d6.jpg",
+        price: 9,
+        brand:'kinsmart',
+        rating: 5,
+        numReviews: 25
+    },
+    {
+        name: '1/32 Dodge charger 1970',
+        category: "1/32 scale models",
+        image: "/images/d7.jpg",
+        price: 3,
+        brand:'kinsmart',
+        rating: 4.5,
+        numReviews: 11
+    },
+    {
+        name: '1/18 Dirt bike',
+        category: "1/18 scale models",
+        image: "/images/d8.jpg",
+        price: 3.5,
+        brand:'chinese',
+        rating: 3,
+        numReviews: 5
+    },
+    {
+        name: '1/18 KTM 1190 Rc8',
+        category: "1/18 scale models",
+        image: "/images/d9.jpg",
+        price: 13,
+        brand:'Bburago',
+        rating: 5,
+        numReviews: 25
+        },
+    {
+        name: '1/32 Mclaren f1',
+        category: "1/32 scale models",
+        image: "/images/d10.jpg",
+        price: 5.5,
+        brand:'kinsmart',
+        rating: 5,
+        numReviews: 15
+    },
+    {
+        name: '1/32 Land Rover Defender',
+        category: "1/32 scale models",
+        image: "/images/d11.jpg",
+        price: 5.5,
+        brand:'kinsmart',
+        rating: 5,
+        numReviews: 20
+    },
+    {
+        name: '1/32 Jeep rangler ',
+        category: "1/32 scale models",
+        image: "/images/d12.jpg",
+        price: 5.5,
+        brand:'kinsmart',
+        rating: 5,
+        numReviews: 19
+    },
+
+
+]
+}
+export default data;
