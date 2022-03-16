eb7a9d482206a06dd588304df54f87cfccfcf440
muhammadzubairqadir <muhammadzubairqadir161@gmail.com>
2022-03-16 15:21:06 +0500

    share react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,98 +1,57 @@
-import React from 'react';
-import {BrowserRouter , Route } from 'react-router-dom';
-
-import './App.css';
+import React from 'react'
 import data from './data';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
 
 function App() {
+    return (
 
-const openMenu = ()=>{
-  document.querySelector(".sidebar").classList.add("open");
-}
-const closeMenu = ()=>{
-   document.querySelector(".sidebar").classList.remove("open");
-}
-
-
-  return (
-    
-   <BrowserRouter>
-
-     <div className="grid-container">
-        <header className="header">
-            <div className="brand">
-                <button onClick={openMenu}>
-                    &#9776;
-                </button>
-                <a href="index.html">amazona</a>
-            </div> 
-            
-            <div className="header-links">
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign In</a>
-
-            </div>  
-        </header>
-
-        <aside className="sidebar">
-            <h3>Shopping Categories</h3>
-            <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-
-
-            <ul className="category">
-              
-                <li>
-                    <a href="index.html">1/12 scale models</a>
-                </li>
-                <li>
-                    <a href="index.html">1/18 scale models</a>
-                </li>
-                <li>
-                    <a href="index.html">1/24 scale models</a>
-                </li>
-                <li>
-                    <a href="index.html">1/32 scale models</a>
-                </li>
-                <li>
-                    <a href="index.html">1/36 scale models</a>
-                </li>
-                <li>
-                    <a href="index.html">1/64 scale models</a>
-                </li>
-                
-            </ul>
-    </aside>
-        <main className="main">
-            <div className="content">
-
-                <Route path="/products/:id" component={ProductScreen} />
-                <Route path="/" exact={true}component={HomeScreen   } />
-
-                <ul className="products">
-                  {
-                    data.products.map(product =>
-                      <li key={product.name}>
-                        <div className="product">
-                            <img className="product-image" src={product.image} alt="product"/>
-                            <div className="product-name"><a href="product.html">{product.name}</a></div>
-                            <div className="product-brand">{product.brand}</div>
-                            <div className="product-price">${product.price}</div>
-                            <div className="product-rating">{product.rating} Sarts ({product.numReviews} reviews)</div>
-                        </div>
-                    </li>)
-                  }
-
-                </ul>
-               </div>
-            </main>
-            <footer className="footer">
-                All right reserved.
-            </footer>   
+        <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">amazona</a>
         </div>
-       </BrowserRouter>
-  );
-}
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      
+      <main>
+        <div>
+          <div className="row center">  
+              {
+                  data.products.map(product =>(
 
-export default App;
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+
+                <img className="medium" 
+                     src={product.image}
+                     alt={product.name} 
+                />
+              </a>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                    <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                  <span> <i className="fa fa-star"></i> </span>
+                </div>
+                <div className="price">${product.price}</div>
+              </div>
+            </div>
+
+                  ))
+              }
+                      </div>
+        </div>
+      </main>
+      <footer className="row center">All right reserved</footer>
+    </div>
+
+    );
+}
+export default App
--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,115 +1,137 @@
 const data = {
     products : [
     {
+        _id: '1',
         name: '1/24 Land Crouser',
         category: "1/24 scale models",
         image: "/images/d1.jpg",
         price: 25,
         brand:'XLG',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        Description: 'Hight quality product',
     },
     {
+        _id: '2',
         name: '1/24 Lamborghini Countach',
         category: "1/24 scale models",
         image: "/images/d2.jpg",
         price: 25,
         brand:'Bburago',
         rating: 5,
-        numReviews: 20
+        numReviews: 20,
+        Description: 'Hight quality product',
     },
     {
+        _id: '3',
         name: '1/27 Chevrolet Colorado ZR2',
         category: "1/27 scale models",
         image: "/images/d3.jpg",
         price: 33,
         brand:'Maisto',
         rating: 4,
-        numReviews: 15
+        numReviews: 15,
+        Description: 'Hight quality product',
     },
     {
+        _id: '4',
         name: '1/32 Audi R8 (v8 engine) 2010 ',
         category: "1/32 scale models",
         image: "/images/d4.jpg",
         price: 4,
         brand:'kinsmart',
         rating: 4.6,
-        numReviews: 9
+        numReviews: 9,
+        Description: 'Hight quality product',
     },
     {
+        _id: '5',
         name: '1/32 GTR R35  ',
         category: "1/32 scale models",
         image: "/images/d5.jpg",
         price: 6,
         brand:'chinese',
         rating: 4,
-        numReviews: 5
+        numReviews: 5,
+        Description: 'Hight quality product',
     },
     {
+        _id: '6',
         name: '1/36 Mercedes-benz SLS(AMG)',
         category: "1/36 scale models",
         image: "/images/d6.jpg",
         price: 9,
         brand:'kinsmart',
         rating: 5,
-        numReviews: 25
+        numReviews: 25,
+        Description: 'Hight quality product',
     },
     {
+        _id: '7',
         name: '1/32 Dodge charger 1970',
         category: "1/32 scale models",
         image: "/images/d7.jpg",
         price: 3,
         brand:'kinsmart',
         rating: 4.5,
-        numReviews: 11
+        numReviews: 11,
+        Description: 'Hight quality product',
     },
     {
+        _id: '8',
         name: '1/18 Dirt bike',
         category: "1/18 scale models",
         image: "/images/d8.jpg",
         price: 3.5,
         brand:'chinese',
         rating: 3,
-        numReviews: 5
+        numReviews: 5,
+        Description: 'Hight quality product',
     },
     {
+        _id: '9',
         name: '1/18 KTM 1190 Rc8',
         category: "1/18 scale models",
         image: "/images/d9.jpg",
         price: 13,
         brand:'Bburago',
         rating: 5,
-        numReviews: 25
+        numReviews: 25,
+        Description: 'Hight quality product',
         },
     {
+        _id: '10',
         name: '1/32 Mclaren f1',
         category: "1/32 scale models",
         image: "/images/d10.jpg",
         price: 5.5,
         brand:'kinsmart',
         rating: 5,
-        numReviews: 15
+        numReviews: 15,
+        Description: 'Hight quality product',
     },
     {
+        _id: '11',
         name: '1/32 Land Rover Defender',
         category: "1/32 scale models",
         image: "/images/d11.jpg",
         price: 5.5,
         brand:'kinsmart',
         rating: 5,
-        numReviews: 20
+        numReviews: 20,
+        Description: 'Hight quality product',
     },
     {
+        _id: '12',
         name: '1/32 Jeep rangler ',
         category: "1/32 scale models",
         image: "/images/d12.jpg",
         price: 5.5,
         brand:'kinsmart',
         rating: 5,
-        numReviews: 19
+        numReviews: 19,
+        Description: 'Hight quality product',
     },
-
-
 ]
 }
 export default data;
